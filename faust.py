0d5e520484f4f94c393c25dac6aafd64fa40f6df
Remi Martel <luxor37@gmail.com>
2023-04-26 23:02:35 -0400

    added gui inputs and moved logic to class

--- faust.py
+++ faust.py
@@ -8,7 +8,8 @@
 # from https://etudier.uqam.ca/recherche-horaires
 from datetime import datetime
 import argparse
-from faust_lib import get_schedule
+import sys
+from faust_lib import Faust
 
 import tkinter as tk
 from tkinter import ttk
@@ -43,26 +44,35 @@ config_section = ttk.Frame(root)
 config_section.pack(padx=10, pady=10, fill='x', expand=True, anchor=tk.N)
 
 # ANNEE
+programme = tk.StringVar()
+programme.set("")
+
+programme_label = ttk.Label(config_section, text="Programme (facultatif):")
+programme_label.grid(column=0, row=0, sticky=tk.E)
+programme_entry = ttk.Entry(config_section, textvariable=programme, width=5)
+programme_entry.grid(column=1, row=0, columnspan=3, sticky=tk.W)
+programme_entry.focus()
+
+# ANNEE
 annee = tk.StringVar()
 annee.set(datetime.now().year)
 
 annee_label = ttk.Label(config_section, text="Annee:")
-annee_label.grid(column=0, row=0, sticky=tk.W)
+annee_label.grid(column=0, row=1, sticky=tk.E)
 annee_entry = ttk.Entry(config_section, textvariable=annee, width=5)
-annee_entry.grid(column=1, row=0, columnspan=3, sticky=tk.W)
-annee_entry.focus()
+annee_entry.grid(column=1, row=1, columnspan=3, sticky=tk.W)
 
 # SESSION
 sessions = [
     {"id": 1, "title": "Hiver"},
-    {"id": 2, "title": "Ete"},
+    {"id": 2, "title": "Été"},
     {"id": 3, "title": "Automne"}]
 
 selected_session = tk.StringVar()
 selected_session.set("1")
 
 session_label = ttk.Label(config_section, text="Session:")
-session_label.grid(column=0, row=1, sticky=tk.W)
+session_label.grid(column=0, row=2, sticky=tk.E)
 
 for j, session in enumerate(sessions):
     r = ttk.Radiobutton(
@@ -71,10 +81,26 @@ for j, session in enumerate(sessions):
         value=session["id"],
         variable=selected_session
     )
-    r.grid(column=j+1, row=1, sticky=tk.W)
+    r.grid(column=j+1, row=2, sticky=tk.W)
 
-button = ttk.Button(root, text='Click Me', command=lambda: get_schedule(
-    annee.get()+selected_session.get()))
+# POLITE CHECKBOX
+polite = tk.StringVar()
+polite.set(True)
+polite_check = ttk.Checkbutton(config_section, text='Polite? (recommendé, mais plus long)', variable=polite, onvalue=True, offvalue=False)
+polite_check.grid(column=1, row=3, columnspan=3, sticky=tk.E)
+
+# LOGS
+output_label = ttk.Label(root, text="Logs:")
+output_label.pack()
+output = tk.Text(root, height=8)
+output.pack(fill='x')
+
+#LOGIC AND BUTTON
+def launchProgram():
+    faust = Faust(annee.get()+selected_session.get(), output, polite.get(), programme.get())
+    faust.get_schedule()
+
+button = ttk.Button(root, text='Générer', command=lambda: launchProgram())
 button.pack()
 
 try:
@@ -84,14 +110,3 @@ try:
 finally:
     if __name__ == '__main__':
         root.mainloop()
-
-
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument(
-#         '-o', '--output', default=CSV_FILE_NAME, help="Fichier CVS")
-#     parser.add_argument('-r', dest='refresh', action='store_true',
-#                         help="Pour retelecharger les horaire à jour")
-#     args = parser.parse_args()
-
-#     get_schedule(args.output, args.refresh)
--- faust_lib.py
+++ faust_lib.py
@@ -2,153 +2,161 @@ from bs4 import BeautifulSoup
 from variables import URL_LISTE_PROGRAMME, URL_DETAIL_COUR, PROGRAMMES, cookies, headers
 import requests, csv, time, re
 
-def get_program_content(session, code, i):
-    print("Downloading programme " + code + " " + str(i) + "/" + str(len(PROGRAMMES)))
+class Faust:
+    def __init__(self, session, text_widget, polite=False, programme="7416"):
+        print(polite)
+        print(programme)
+        self.text_widget = text_widget
+        self.session = session
+        self.polite = polite
+        if programme != "":
+            self.programmes = [programme]
+        else:
+            self.programmes = PROGRAMMES
 
-    post_items = {
-        'pn_trim_num': session,
-        'pc_type_offre': 'P',
-        'pc_code_prog': code,
-        'pc_titre': '',
-        'pc_groupe': '',
-        'pc_dept': '',
-        'pa_no_jour': '',
-        'pa_no_creneau': '',
-        'pa_campus': '',
-        'pa_cycle': '',
-        'pa_type_ens': '',
-        'pa_type_horaire': '',
-        'pa_type_cours': '',
-        'pc_min_place': '',
-        'pn_max_cours': 500,
-        'pc_format': 'HTML_AVEC_CART_HEADER_BACK_BUTTON',
-    }
-    
-    resp = requests.post(URL_LISTE_PROGRAMME, data=post_items, cookies=cookies, headers=headers, stream=True)
-    return resp.content.decode('latin-1').encode('utf-8')
+    def write_to_gui(self, text):
+        print(text)
+        self.text_widget.insert('end', text+"\n")
+        self.text_widget.see('end')
 
+    def get_program_content(self, code, i):
+        self.write_to_gui("Downloading programme " + code + " " + str(i) + "/" + str(len(PROGRAMMES)))
 
-def get_cours_content(session, sigle, i, total):
-    print("Downloading cours " + sigle + " " + str(i) + "/" + str(total))
-    
-    post_items = {
-        'pn_trim_num': session,
-        'pc_niveau': 'D',
-        'pc_format': 'HTML_AVEC_CART_HEADER_BACK_BUTTON_APPEND_COMPRIME',
-        'pc_sigle': sigle,
-        'pc_code_prog': '',
-    }
-    
-    resp = requests.post(URL_DETAIL_COUR, data=post_items, cookies=cookies, headers=headers, stream=True)
-    return resp.content.decode('latin-1').encode('utf-8')
-
-
-def get_classes_from_html(session, polite, programme):
-    tous_les_cours = []
-    programmes = PROGRAMMES
-    if programme != "":
-        programmes = [programme]
-    for i, prog in enumerate(programmes):
-        # TEST
-        # if i >= 1:
-        #     break
-
-        if polite:
-            time.sleep(0.5)
-
-        soup = BeautifulSoup(get_program_content(session, prog, i+1), 'html.parser')
-        all_cours = soup.find_all('a')
+        post_items = {
+            'pn_trim_num': self.session,
+            'pc_type_offre': 'P',
+            'pc_code_prog': code,
+            'pc_titre': '',
+            'pc_groupe': '',
+            'pc_dept': '',
+            'pa_no_jour': '',
+            'pa_no_creneau': '',
+            'pa_campus': '',
+            'pa_cycle': '',
+            'pa_type_ens': '',
+            'pa_type_horaire': '',
+            'pa_type_cours': '',
+            'pc_min_place': '',
+            'pn_max_cours': 500,
+            'pc_format': 'HTML_AVEC_CART_HEADER_BACK_BUTTON',
+        }
         
-        for j, cours in enumerate(all_cours):
-            if j != 0:
-                # cours =  <a onclick="det_horaire2('SOM4500' , 'pc_niveau=D&amp;pn_trim_num=20191&amp;pc_format...
-                a = re.split(' ', cours.string)
-                tous_les_cours.append(a[0])
-    
-    return sorted(set(tous_les_cours))
+        resp = requests.post(URL_LISTE_PROGRAMME, data=post_items, cookies=cookies, headers=headers, stream=True)
+        return resp.content.decode('latin-1').encode('utf-8')
 
 
-def get_classes(session, polite, programme):
-    sigles = get_classes_from_html(session, polite, programme)
-    classes = []
-
-    for i, sigle in enumerate(sigles):
-        # TEST
-        # if i >= 1:
-        #     break
-
-        if polite:
-            time.sleep(0.5)
-
-        page_du_cours = BeautifulSoup(get_cours_content(session, sigle, i+1, len(sigles)), 'html.parser')
+    def get_cours_content(self, sigle, i, total):
+        self.write_to_gui("Downloading cours " + sigle + " " + str(i) + "/" + str(total))
         
-        titre = page_du_cours.find('h2').text.split(' ', 2)[2]
-        rows = page_du_cours.find_all('tr')
-
-        prof = ""
-        presentiel = ""
-        note = ""
-        dates = ""
-        groupes_info_incomplet = []
-        last_group_num = ""
-        for j, row in enumerate(rows):
-            if j == 0:  # headers row
-                continue
-
-            columns = row.find_all('td')
-            if len(columns) == 6:
-                groupe_info = get_group_info(columns, last_group_num)
-                last_group_num = groupe_info['groupe']
-                groupes_info_incomplet.append(groupe_info)
-            else:
-                if "Enseignants" in row.text or "Enseignant" in row.text:
-                    prof = row.text.split(':')[1]
-                elif "Mode d'enseignement en ligne" in row.text:
-                    presentiel = row.text.split(':')[1]
-                elif "Remarque" in row.text:
-                    note += " " + row.text.split(':')[1]
-                elif "Periode" in row.text:
-                    dates = row.text.split(':')[1]
-                # else:
-                    # note += " " + row.text
-
-        for groupe in groupes_info_incomplet:
-            groupe['prof'] = prof
-            groupe['presentiel'] = presentiel
-            groupe['note'] = note
-            groupe['dates'] = dates
-            groupe['titre'] = titre
-            groupe['sigle'] = sigle
-            classes.append(groupe)
-
-    return classes
+        post_items = {
+            'pn_trim_num': self.session,
+            'pc_niveau': 'D',
+            'pc_format': 'HTML_AVEC_CART_HEADER_BACK_BUTTON_APPEND_COMPRIME',
+            'pc_sigle': sigle,
+            'pc_code_prog': '',
+        }
+        
+        resp = requests.post(URL_DETAIL_COUR, data=post_items, cookies=cookies, headers=headers, stream=True)
+        return resp.content.decode('latin-1').encode('utf-8')
 
 
-def save_as_csv(classes):
-    fieldnames = ['groupe', 'jour', 'debut', 'fin', 'salle', 'prof', 'type', 'presentiel', 'dates', 'sigle', 'titre', 'note']
-    with open('liste-cours.csv', 'w', encoding='UTF-8') as csv_f:
-        writer = csv.DictWriter(csv_f, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(classes)
-    print("Le fichier CSV a ete ecrit: " + '"./liste-cours.csv"')
+    def get_classes_from_html(self):
+        tous_les_cours = []
+
+        for i, prog in enumerate(self.programmes):
+            if self.polite:
+                time.sleep(0.5)
+
+            html = BeautifulSoup(self.get_program_content(prog, i+1), 'html.parser')
+            
+            for j, cours in enumerate(html.find_all('a')):
+                if j != 0:
+                    # cours =  <a onclick="det_horaire2('SOM4500' , 'pc_niveau=D&amp;pn_trim_num=20191&amp;pc_format...
+                    a = re.split(' ', cours.string)
+                    tous_les_cours.append(a[0])
+        
+        return sorted(set(tous_les_cours))
 
 
-def get_group_info(columns, last_group=""):
-    # Sometime there's multiple class for the same group, but the group number isn't repeated, it's empty instead
-    if columns[0].text.strip():
-        groupe = columns[0].text.strip()
-    else:
-        groupe = last_group
-    return {
-        'groupe': groupe,
-        'jour': columns[1].text.strip(),
-        'debut': columns[2].text,
-        'fin': columns[3].text,
-        'salle': columns[4].text.strip(),
-        'type': columns[5].text,
-    }
+    def get_classes(self):
+        sigles = self.get_classes_from_html()
+        classes = []
+
+        for i, sigle in enumerate(sigles):
+            if self.polite:
+                time.sleep(0.5)
+
+            page_du_cours = BeautifulSoup(self.get_cours_content(sigle, i+1, len(sigles)), 'html.parser')
+            
+            titre = page_du_cours.find('h2').text.split(' ', 2)[2]
+            rows = page_du_cours.find_all('tr')
+
+            prof = ""
+            presentiel = ""
+            note = ""
+            dates = ""
+            groupes_info_incomplet = []
+            last_group_num = ""
+            for j, row in enumerate(rows):
+                if j == 0:  # headers row
+                    continue
+
+                columns = row.find_all('td')
+                if len(columns) == 6:
+                    groupe_info = self.get_group_info(columns, last_group_num)
+                    last_group_num = groupe_info['groupe']
+                    groupes_info_incomplet.append(groupe_info)
+                else:
+                    if "Enseignants" in row.text or "Enseignant" in row.text:
+                        prof = row.text.split(':')[1]
+                    elif "Mode d'enseignement en ligne" in row.text:
+                        presentiel = row.text.split(':')[1]
+                    elif "Remarque" in row.text:
+                        note += " " + row.text.split(':')[1]
+                    elif "Periode" in row.text:
+                        dates = row.text.split(':')[1]
+                    # else:
+                        # note += " " + row.text
+
+            for groupe in groupes_info_incomplet:
+                groupe['prof'] = prof
+                groupe['presentiel'] = presentiel
+                groupe['note'] = note
+                groupe['dates'] = dates
+                groupe['titre'] = titre
+                groupe['sigle'] = sigle
+                classes.append(groupe)
+
+        return classes
 
 
-def get_schedule(session, polite=False, programme="7416"):
-    classes = get_classes(session, polite, programme)
-    save_as_csv(classes)
+    def save_as_csv(self, classes):
+        fieldnames = ['groupe', 'jour', 'debut', 'fin', 'salle', 'prof', 'type', 'presentiel', 'dates', 'sigle', 'titre', 'note']
+        with open('liste-cours.csv', 'w', encoding='UTF-8') as csv_f:
+            writer = csv.DictWriter(csv_f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(classes)
+        self.write_to_gui("Le fichier CSV a ete ecrit: " + '"./liste-cours.csv"')
+
+
+    def get_group_info(self, columns, last_group=""):
+        # Sometime there's multiple class for the same group, but the group number isn't repeated, it's empty instead
+        if columns[0].text.strip():
+            groupe = columns[0].text.strip()
+        else:
+            groupe = last_group
+        return {
+            'groupe': groupe,
+            'jour': columns[1].text.strip(),
+            'debut': columns[2].text,
+            'fin': columns[3].text,
+            'salle': columns[4].text.strip(),
+            'type': columns[5].text,
+        }
+
+
+    def get_schedule(self):
+        print('test')
+        self.save_as_csv(self.get_classes())
+
+
